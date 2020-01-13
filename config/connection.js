de21767cb096d031c53aeb015c6453261cea63b9
Eduardo Neto <edu.neto@icloud.com>
2020-01-13 18:23:17 +0000

    Altered database connection credentials

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   port: 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: "burgers_db"
+  database: "burger_prod_db"
 });
 
 // Connect to the database
